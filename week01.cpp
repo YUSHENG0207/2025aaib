b94933d703338326e44d74b09659d2391b64615e
YUSHENG0207 <yusheng0207@gmail.com>
2025-02-26 12:01:31 +0800

    week01

---
+++ week01.cpp
@@ -0,0 +1,12 @@
+///week01-1.cpp 瘋狂程設 兩數相加
+
+#include<stdio.h>///檔名不能錯
+///standard input/output
+///有stdio.h的外掛，才能用scanf() printf()
+int main();
+{
+    int a, b;///整數
+    scanf("%d",&a);///讀數
+    scanf("%d",&b);///同上
+    printf("%d",a+b);///印出
+}
